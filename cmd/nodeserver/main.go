d89feeceb9f5405c3fbe67e17d858ba8a6d14d2a
Chad Hardin <chardin@cirglo.com>
2025-04-01 20:45:35 -0700

    Added etcd reporting

--- cmd/nodeserver/main.go
+++ cmd/nodeserver/main.go
@@ -1,9 +1,91 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
+	"github.com/cirglo.com/dfs/pkg/node"
+	"github.com/sirupsen/logrus"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"os"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	logLevelFlag := flag.String("log-level", "info", "Log Level")
+	idFlag := flag.String("id", "default-id", "Node ID")
+	locationFlag := flag.String("location", "/", "Node Location")
+	dirFlag := flag.String("dir", "/tmp", "Node Directory")
+	healthCheckIntervalFlag := flag.Duration("health-check-interval", 1*time.Minute, "Health Check Interval")
+	crcCheckIntervalFlag := flag.Duration("crc-check-interval", 24*time.Hour, "CRC Check Interval")
+	leaseDurationFlag := flag.Duration("lease-duration", 2*time.Minute, "Lease Duration")
+	etcdIntervalFlag := flag.Duration("etcd-interval", 1*time.Minute, "ETCD Interval")
+	etcdTimeoutFlag := flag.Duration("etcd-timeout", 5*time.Second, "ETCD Timeout")
+
+	flag.Parse()
+
+	log := logrus.New()
+	logLevel, err := logrus.ParseLevel(*logLevelFlag)
+	if err != nil {
+		log.WithError(err).WithField("level", *logLevelFlag).Fatalf("Invalid log level")
+	}
+
+	log.SetLevel(logLevel)
+
+	dir, err := os.Stat(*dirFlag)
+	if err != nil {
+		log.WithError(err).WithField("dir", *dirFlag).Fatal("Directory does not exist")
+	}
+
+	etcdOpts := node.EtcdOpts{
+		ID:            *idFlag,
+		Host:          "localhost:2379",
+		LeaseDuration: *leaseDurationFlag,
+		ContextFactory: func() (context.Context, context.CancelFunc) {
+			return context.WithTimeout(context.Background(), *etcdTimeoutFlag)
+		},
+		Client: &clientv3.Client{
+			Cluster:     nil,
+			KV:          nil,
+			Lease:       nil,
+			Watcher:     nil,
+			Auth:        nil,
+			Maintenance: nil,
+			Username:    "",
+			Password:    "",
+		},
+	}
+
+	etcd, err := node.NewEtcd(etcdOpts)
+	if err != nil {
+		log.WithError(err).Fatal("Failed to create etcd client")
+	}
+
+	go func() {
+		ticker := time.NewTicker(*etcdIntervalFlag)
+		defer ticker.Stop()
+		for range ticker.C {
+			err := etcd.Report()
+			if err != nil {
+				log.WithError(err).Fatal("Failed to report to etcd")
+			} else {
+				log.Info("Reported to etcd successfully")
+			}
+		}
+	}()
+
+	serviceOpts := node.ServiceOpts{
+		Logger:              nil,
+		ID:                  *idFlag,
+		Location:            *locationFlag,
+		Dir:                 dir,
+		HealthCheckInterval: *healthCheckIntervalFlag,
+		ValidateCRCInterval: *crcCheckIntervalFlag,
+	}
+
+	_, err = node.NewService(serviceOpts)
+	if err != nil {
+		log.WithError(err).Fatal("Failed to create service")
+	}
+
 }
